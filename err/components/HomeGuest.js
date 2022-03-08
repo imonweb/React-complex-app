cceb8a602236190cfca3952748b310a9fb3b2949
imonweb <contact@imonweb.co.uk>
2022-03-08 16:02:53 +0000

    user admin created

--- err/components/HomeGuest.js
+++ err/components/HomeGuest.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react"
+import React from "react"
 import Page from "./Page"
 import Axios from "axios"
 
 function HomeGuest() {
-  const [username, setUsername] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  // const [username, setUsername] = useState()
+  // const [email, setEmail] = useState()
+  // const [password, setPassword] = useState()
 
   async function handleSubmit(e) {
     e.preventDefault()
     try {
       await Axios.post("http://localhost:8080/register", { username, email, password })
-      console.log("User was successfully created.")
+      console.log("user was successfully created.")
     } catch (e) {
       console.log("There was an error.")
     }
   }
 
   return (
-    <Page title="Welcome!" wide={true}>
+    <Page title="Welcome" wide={true}>
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
           <h1 className="display-3">Remember Writing?</h1>
@@ -30,19 +30,19 @@ function HomeGuest() {
               <label htmlFor="username-register" className="text-muted mb-1">
                 <small>Username</small>
               </label>
-              <input onChange={(e) => setUsername(e.target.value)} id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
+              <input id="username-register" name="username" className="form-control" type="text" placeholder="Pick a username" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="email-register" className="text-muted mb-1">
                 <small>Email</small>
               </label>
-              <input onChange={(e) => setEmail(e.target.value)} id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
+              <input id="email-register" name="email" className="form-control" type="text" placeholder="you@example.com" autoComplete="off" />
             </div>
             <div className="form-group">
               <label htmlFor="password-register" className="text-muted mb-1">
                 <small>Password</small>
               </label>
-              <input onChange={(e) => setPassword(e.target.value)} id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
+              <input id="password-register" name="password" className="form-control" type="password" placeholder="Create a password" />
             </div>
             <button type="submit" className="py-3 mt-4 btn btn-lg btn-success btn-block">
               Sign up for ComplexApp
