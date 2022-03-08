a3052503b577d31747cc9762d26e7d058abfb56a
imonweb <contact@imonweb.co.uk>
2022-03-08 11:10:10 +0000

    router

--- app/Main.js
+++ app/Main.js
@@ -1,16 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-function ExampleComponent() {
+// My Components
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
+
+function Main() {
   return (
-    <div>
-      <h1>This is our app!</h1>
-      <p>The sky is blue and the grass is green.</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomeGuest />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
-ReactDOM.render(<ExampleComponent />, document.querySelector("#app"))
+ReactDOM.render(<Main />, document.querySelector("#app"))
 
 if (module.hot) {
   module.hot.accept()
