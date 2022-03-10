bfd4ae6bda136af59b32651c5661917103673f85
imonweb <contact@imonweb.co.uk>
2022-03-10 00:01:27 +0000

    fixed

---
+++ app/components/HeaderLoggedIn.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useContext } from "react"
+import { Link } from "react-router-dom"
+import DispatchContext from "../DispatchContext"
+
+function HeaderLoggedIn(props) {
+  const appDispatch = useContext(DispatchContext)
+
+  function handleLogout() {
+    appDispatch({ type: "logout" })
+    localStorage.removeItem("complexappToken")
+    localStorage.removeItem("complexappUsername")
+    localStorage.removeItem("complexappAvatar")
+  }
+
+  return (
+    <div className="flex-row my-3 my-md-0">
+      <a href="#" className="text-white mr-2 header-search-icon">
+        <i className="fas fa-search"></i>
+      </a>
+      <span className="mr-2 header-chat-icon text-white">
+        <i className="fas fa-comment"></i>
+        <span className="chat-count-badge text-white"> </span>
+      </span>
+
+      <a href="#" className="mr-2">
+        <img className="small-header-avatar" src="https://www.imonweb.co.uk/avatar1.png" width="34px" />
+        {/* <img className="small-header-avatar" src={localStorage.getItem("complexappAvatar")} width="32px"/> */}
+      </a>
+      <Link className="btn btn-sm btn-success mr-2" to="/create-post">
+        Create Post
+      </Link>
+      <button onClick={handleLogout} className="btn btn-sm btn-secondary">
+        Sign Out
+      </button>
+    </div>
+  )
+}
+
+export default HeaderLoggedIn
